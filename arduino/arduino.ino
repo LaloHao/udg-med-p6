16b1d533f1b9c652e90514c216a7adaa16566f49
Eduardo Vazquez <lalohao@gmail.com>
2014-11-09 13:08:05 -0600

    Codigo corregido

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,21 +1,91 @@
-int pwmPin = 9, command, tmpVoltaje;
-float voltajes[] = {0, 0.75, 1.5, 2.25, 3, 3.75, 4.5};
+const int pwmPin = 9;
+int command;
+float tmpVoltaje, voltajes[] = {0, 0.75, 1.5, 2.25, 3, 3.75, 4.5};
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(pwmPin, OUTPUT);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
 }
 
 void loop()
 {
-  tmpVoltaje = 0;
   while(Serial.available() > 0)
   {
-    command = Serial.read()-30;
-    if(command >= 1 && command <= 7)
-      tmpVoltaje = 255 * (voltajes[1] / 5);
-    analogWrite(pwmPin, tmpVoltaje);
+    command = Serial.read();
+    switch(command)
+    {
+      case '1':
+        tmpVoltaje = voltajes[0];
+        tmpVoltaje = tmpVoltaje * (255/ 5);
+        analogWrite(pwmPin, tmpVoltaje);
+        delay(500);
+        Serial.print(analogRead(A0));
+        delay(250);
+        Serial.print(analogRead(A1));
+        break;
+      case '2':
+        tmpVoltaje = voltajes[1];
+        tmpVoltaje = tmpVoltaje * (255/ 5);
+        analogWrite(pwmPin, tmpVoltaje);
+        delay(500);
+        Serial.print(analogRead(A0));
+        delay(250);
+        Serial.print(analogRead(A1));
+        break;
+      case '3':
+        tmpVoltaje = voltajes[2];
+        tmpVoltaje = tmpVoltaje * (255/ 5);
+        analogWrite(pwmPin, tmpVoltaje);
+        delay(500);
+        Serial.print(analogRead(A0));
+        delay(250);
+        Serial.print(analogRead(A1));
+        break;
+      case '4':
+        tmpVoltaje = voltajes[3];
+        tmpVoltaje = tmpVoltaje * (255/ 5);
+        analogWrite(pwmPin, tmpVoltaje);
+        delay(500);
+        Serial.print(analogRead(A0));
+        delay(250);
+        Serial.print(analogRead(A1));
+        break;
+      case '5':
+        tmpVoltaje = voltajes[4];
+        tmpVoltaje = tmpVoltaje * (255/ 5);
+        analogWrite(pwmPin, tmpVoltaje);
+        delay(500);
+        Serial.print(analogRead(A0));
+        delay(250);
+        Serial.print(analogRead(A1));
+        break;
+      case '6':
+        tmpVoltaje = voltajes[5];
+        tmpVoltaje = tmpVoltaje * (255/ 5);
+        analogWrite(pwmPin, tmpVoltaje);
+        delay(500);
+        Serial.print(analogRead(A0));
+        delay(250);
+        Serial.print(analogRead(A1));
+        break;
+      case '7':
+        tmpVoltaje = voltajes[6];
+        tmpVoltaje = tmpVoltaje * (255/ 5);
+        analogWrite(pwmPin, tmpVoltaje);
+        delay(500);
+        Serial.print(analogRead(A0));
+        delay(250);
+        Serial.print(analogRead(A1));
+        break;
+      default:
+        tmpVoltaje = voltajes[0];
+        tmpVoltaje = tmpVoltaje * (255/ 5);
+        analogWrite(pwmPin, tmpVoltaje);
+        break;
+    }
   }
 }
 
+//voltajes[command-1]
